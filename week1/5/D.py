a8017ba4149a426e7e98bd075c1734fda8519df0
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2024-01-07 22:53:29 +0900

    5

---
+++ week1/5/D.py
@@ -0,0 +1,34 @@
+# https://www.acmicpc.net/problem/5397
+import sys
+
+n = int(sys.stdin.readline())
+
+for _ in range(n):
+
+    left = []
+    right = []
+
+    pwd = list(sys.stdin.readline().rstrip())
+
+    for char in pwd:
+
+        if char == ">":
+            if right:
+                left.append(right.pop()) 
+
+        elif char=="<":
+            if left:
+                right.append(left.pop())
+
+        elif char=="-":
+            if left:
+                left.pop()
+
+        else:
+            left.append(char)
+
+    print("".join(left) + "".join(reversed(right)))
+
+# 2
+# <<BP<A>>Cd-
+# ThIsIsS3Cr3t
