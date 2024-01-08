45935848264e63567914fbad7b5356c5900a163a
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2024-01-08 22:29:40 +0900

    5-D-rs

--- week1/5/D.rs
+++ week1/5/D.rs
@@ -1,2 +1,59 @@
 // https://www.acmicpc.net/problem/5397
 
+use std::io::{stdin, BufRead};
+fn main() {
+    let mut answer= String::new();
+
+    let std= stdin();
+    let mut buf= std.lock().lines();
+    let input= buf.next().unwrap().unwrap();
+
+    let t: i32= input.trim().parse().unwrap();
+    let mut inputes: Vec<char>;
+
+    for _ in 0..t{
+        let mut left: Vec<char> = Vec::new();
+        let mut right: Vec<char> = Vec::new();
+
+        let input2= buf.next().unwrap().unwrap();
+        inputes= input2.trim().chars().collect();
+        
+        for c in inputes{
+            match c {
+                '<' => {
+                    if !left.is_empty(){
+                        right.push(left.pop().unwrap());
+                    }
+                },
+
+                '>' => {
+                    if !right.is_empty() {
+                        left.push(right.pop().unwrap());
+                    }
+                },
+
+                '-' => {
+                    if !left.is_empty() {
+                        left.pop();
+                    }
+                },
+
+                _ => left.push(c),
+            }
+        }
+
+        while !left.is_empty() {
+            right.push(left.pop().unwrap());
+        }
+
+        while !right.is_empty() {
+            answer.push(right.pop().unwrap());   
+        }
+
+
+// 2
+// <<BP<A>>Cd-
+// ThIsIsS3Cr3t
+        println!("{}", answer);
+    }
+}
