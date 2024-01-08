5a1dc266e89d05b2bdc76587a83e871ef98675a6
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2024-01-08 20:53:17 +0900

    5

--- week1/5/D.java
+++ week1/5/D.java
@@ -4,49 +4,64 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main {
+public class D {
     public static void main(String[] args) throws IOException {
         BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb= new StringBuilder();
 
         final int T= Integer.parseInt(br.readLine());
-        String[] input;
+        String[] inputs;
 
         for (int i=0; i<T; i++){
             Stack<String> left= new Stack<>();
             Stack<String> right= new Stack<>();
-            input = br.readLine().split("");
 
-            for (var str : input) {
+            // to chars array
+            inputs = br.readLine().split("");
+
+            for (var str : inputs) {
                 switch (str) {
                     case "<":
+                    // if left values are..
                         if(!left.isEmpty()){
+                            // transfer from left to right 
                             right.push(left.pop());
                         }
                         break;
+
                     case ">":
+                    //  right values are..
                         if (!right.isEmpty()) {
+                            // transfer from right to left                             
                             left.push(right.pop());
                         }
                         break;
+                        
                     case "-":
                         if (!left.isEmpty()) {
                             left.pop();
                         }
                         break;
+                    
                     default:
                         left.push(str);
                 }
             }
 
+            
+            // if left values remain..
             while (!left.isEmpty()) {
                 right.push(left.pop());
             }
+
+            // consume right value using pop
             while (!right.isEmpty()) {
+                // new Stringbuilder
                 sb.append(right.pop());
             }
             sb.append("\n");
         }
+        
         System.out.println(sb.toString());
     }
 }
