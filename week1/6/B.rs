7160a8b6686431056731c5ed80c9fa96b4572905
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2024-01-09 20:47:03 +0900

    15903

--- week1/6/B.rs
+++ week1/6/B.rs
@@ -1,5 +1,95 @@
 // https://www.acmicpc.net/problem/15903
+use std::{io::{stdin, BufRead}, collections::{BinaryHeap, binary_heap}};
+use std::cmp;
 
+fn main() {
+    let std= stdin();
+    let mut buf= std.lock().lines();
+    
+    let input= buf.next().unwrap().unwrap();
+    let nms: Vec<i32>= input.trim().split_whitespace().map(|f| f.parse().unwrap()).collect();
+    
+    let input2= buf.next().unwrap().unwrap();
+    let As: Vec<i32>= input2.trim().split_whitespace().map(|f| f.parse().unwrap()).collect();
+
+    let n= nms[0];
+    let m= nms[1];
+    
+    let mut heap: BinaryHeap<i32>= BinaryHeap::new();
+    for a in As{
+        heap.push(a * -1);
+    }
+
+    for _ in 0..m{
+        let mut mini= i32::MAX;
+        let mut sec_mini= 0;
+        mini= heap.pop().unwrap();
+        sec_mini= heap.pop().unwrap();
+        let s: i32=  (mini+sec_mini) * -1;
+        heap.push(s * -1);
+        heap.push(s * -1);
+    }
+
+    let answer: i32= heap.iter().map(|f| f * -1).sum();
+
+    println!("{}", answer);
+}
+
+
+// mini heap
+use std::{io::{stdin, BufRead}, collections::{BinaryHeap, binary_heap}};
+use std::cmp;
+
+fn main() {
+    let std= stdin();
+    let mut buf= std.lock().lines();
+    
+    let input= buf.next().unwrap().unwrap();
+    let nms: Vec<u32>= input.trim().split_whitespace().map(|f| f.parse().unwrap()).collect();
+    
+    let input2= buf.next().unwrap().unwrap();
+    let As: Vec<u32>= input2.trim().split_whitespace().map(|f| f.parse().unwrap()).collect();
+
+    let n= nms[0];
+    let m= nms[1];
+    
+    let mut heap: BinaryHeap<u32>= BinaryHeap::new();
+    for a in As{
+        heap.push(a);
+    }
+
+    for _ in 0..m{
+        let mut mini= u32::MAX;
+        let mut sec_mini= 0;
+
+        {
+            let mut mini_local= heap.pop().unwrap();
+            let mut stack: Vec<u32>= Vec::new();
+            
+            for _ in 1..heap.len(){
+                mini_local= cmp::min(mini, heap.pop().unwrap());
+                stack.push(heap.pop().unwrap());
+            }
+            stack.sort();
+            mini= mini_local;
+            sec_mini= stack[0];
+        }
+
+        println!("{}: {}", mini, sec_mini);
+        let s: u32=  mini+sec_mini;
+        heap.push(s);
+        heap.push(s);
+    }
+
+    for e in &heap {
+        print!("{} ", e);
+    }
+    let answer: u32= heap.iter().sum();
+
+    println!("{}", answer);
+}
+
+// Max heap
 use std::cmp::Reverse;
 use std::{io::{stdin, BufRead}, collections::BinaryHeap};
 fn main() {
