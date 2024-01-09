7a90e736326ef2fd85f6d72722ed358102a26f3b
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2024-01-09 19:45:14 +0900

    6-15903

--- week1/6/B.rs
+++ week1/6/B.rs
@@ -1,8 +1,42 @@
 // https://www.acmicpc.net/problem/15903
+
+use std::cmp::Reverse;
+use std::{io::{stdin, BufRead}, collections::BinaryHeap};
+fn main() {
+    let std= stdin();
+    let mut buf= std.lock().lines();
+    
+    let input= buf.next().unwrap().unwrap();
+    let nms: Vec<u32>= input.trim().split_whitespace().map(|f| f.parse().unwrap()).collect();
+    
+    let input2= buf.next().unwrap().unwrap();
+    let mut As: Vec<u32>= input2.trim().split_whitespace().map(|f| f.parse().unwrap()).collect();
+
+    let n= nms[0];
+    let m= nms[1];
+    
+    let mut heap: BinaryHeap<Reverse<u32>>= BinaryHeap::new();
+    
+
+    for a in As{
+        heap.push(Reverse(a));
+    }
+
+    for _ in 0..m{    
+        let s: u32= heap.pop().unwrap().0 + heap.pop().unwrap().0;
+        heap.push(Reverse(s));
+        heap.push(Reverse(s));
+    }
+
+    let answer: u32= heap.iter().map(|f| f.0).sum();
+    println!("{}", answer);
+}
+
+
+// My code
 // test case 1,2은 통과
 // 1: https://play.rust-lang.org/?version=stable&mode=debug&edition=2021&gist=ff4d6eae5cb4b5ab1f882a5547e6f49e
 // 2: https://play.rust-lang.org/?version=stable&mode=debug&edition=2021&gist=7c7f2ae91437c70827836b8ba3897d6f
-
 use std::io::{stdin, BufRead};
 fn main() {
     let std= stdin();
