b6a96da2dff3b71224474abee3e75dc21d954e50
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2024-01-11 21:44:24 +0900

    2-2 rs

---
+++ week2/2/A.rs
@@ -0,0 +1,40 @@
+// https://leetcode.com/problems/subsets/
+// source code: https://github.com/Ainevsia/Leetcode-Rust/blob/main/78.%20Subsets/src/main.rs
+
+struct Solution{}
+impl Solution {
+    pub fn subsets(mut nums: Vec<i32>) -> Vec<Vec<i32>>{
+        if nums.len() == 0 {
+            return vec![vec![]];
+        }
+        let last= nums.pop().unwrap();
+        let remain_subsets= Self::subsets(nums);
+        let mut res= remain_subsets.clone();
+        for mut v in remain_subsets{
+            v.push(last);
+            res.push(v);
+        }
+
+        res
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn basic() {
+        assert_eq!(Solution::subsets(vec![1,2,3]), vec![
+            vec![3],
+            vec![1],
+            vec![2],
+            vec![1,2,3],
+            vec![1,3],
+            vec![2,3],
+            vec![1,2],
+            vec![]
+        ]);
+    }
+}
