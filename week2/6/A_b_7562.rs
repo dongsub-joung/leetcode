8abb6e58f2fc0c1e9f626aad47d79d35aced32a9
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2024-01-16 11:28:51 +0900

    owership err

--- week2/6/A_b_7562.rs
+++ week2/6/A_b_7562.rs
@@ -1,8 +1,9 @@
 // https://www.acmicpc.net/problem/7562
-
+// 재귀가 있어 러스트로 못푸는 문제, 소유권 에러가 난다.
 use std::io::{stdin, BufReader, BufRead};
+use std::collections::VecDeque;
 
-pub struct Point{
+struct Point{
     x: i32,
     y: i32,
     cnt: i32,
@@ -17,20 +18,58 @@ impl Point {
     }
 }
 
-fn main() {
+pub fn bfs(arr: Vec<i32>, points: Vec<Point>, max_len_one_side: usize) -> i32{
     let range_x= [-1, -2, -2, -1, 1, 2, 2, 1];
     let range_y= [-2, -1, 1, 2, 2, 1, -1, -2];
-    let mut results: Vec<usize>= Vec::new();
+
+    let mut deque: VecDeque<&Point> = VecDeque::new();
+    deque.push_back(&points[0]);
+
+    let mut visited: Vec<Vec<bool>>= Vec::new();
+    visited[points[0].x as usize][points[0].y as usize]= true;
+
+    while !deque.is_empty() {
+        let point= deque.pop_front().unwrap();
+
+        if &point.x == &points[1].x 
+            && &point.y == &points[1].y{
+                return point.cnt;
+        }
+
+        for i in 0..8 {
+            let dx= point.x + range_x[i];
+            let dy= point.y + range_y[i];
+
+            if dx < 0 || dy< 0 
+                || dx >= max_len_one_side as i32 || dy>= max_len_one_side as i32{
+                continue;
+            }
+
+            if !visited[dx as usize][dy as usize] {
+                visited[dx as usize][dy as usize]= true;
+                deque.push_back(&Point::new_wiht_cnt(dx, dy, point.cnt + 1));
+            }
+        }
+
+    }
+    return -1;
+}
+
+fn main() {
+
+    let mut results: Vec<i32>= Vec::new();
+    let mut two_dimention_arrs: Vec<Vec<i32>>= Vec::new();
+    let mut points: Vec<Point>= Vec::new();
+    let mut arr: Vec<i32>= Vec::new();
 
     let std= stdin();
     let mut buf= std.lock().lines();
     let input= buf.next().unwrap().unwrap();
     let input_n: usize= input.trim().parse().unwrap();
 
-    let mut arr:[i32; 0];
     for _ in 0..input_n{
         let max_len_one_side: usize= buf.next().unwrap().unwrap().parse().unwrap();
-        
+
         let current_koma_xy: Vec<i32>= buf.next().unwrap().unwrap()
             .trim().split_whitespace().map(|f| f.parse().unwrap()).collect();
         let will_koma_xy: Vec<i32>= buf.next().unwrap().unwrap()
@@ -40,10 +79,15 @@ fn main() {
         const Y: usize= 0;
         if &current_koma_xy[X] == &will_koma_xy[X] 
             && &current_koma_xy[Y] == &will_koma_xy[Y]{
-            results.push(0_usize);
+            results.push(0_i32);
         }
 
-        // progress
+        // value movedd
+        points.push(Point::new(current_koma_xy[X], current_koma_xy[Y]));
+        points.push(Point::new(will_koma_xy[X], will_koma_xy[Y]));
 
+        // progress
+        
+        results.push(bfs(arr.clone(), points, max_len_one_side,));
     }
 }
