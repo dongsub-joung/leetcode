28285219da90d212744f82d51bc34bcf0c9dd816
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2024-01-22 19:55:00 +0900

    rs

--- week3/5/A_53.rs
+++ week3/5/A_53.rs
@@ -2,9 +2,7 @@
 
 use std::cmp::max;
 
-struct Solution{
-
-}
+struct Solution{}
 impl Solution {
     pub fn max_sub_array(nums: Vec<i32>) -> i32 {
         let mut answer= i32::MIN;
@@ -20,7 +18,6 @@ impl Solution {
 }
 fn main() {
     let nums= Vec::from([-2,1,-3,4,-1,2,1,-5,4]);
-    // let nums= Vec::from([1]);
     let result= Solution::max_sub_array(nums);
     println!("{}", result);
 }
