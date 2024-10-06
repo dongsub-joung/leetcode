2f017a45a7c0311f9c9e83ebe3a4892b68bd4242
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2024-10-06 14:56:31 +0900

    done

--- week0/TwoSum_1/lib.rs
+++ week0/TwoSum_1/lib.rs
@@ -2,7 +2,20 @@ struct Solution{}
 
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut i= 1;
+        let mut pivots= Vec::new();
         
+        for n in &nums{
+            let nums_c= nums.clone();
+            if n+nums_c[i] == target{
+                pivots.push((i-1) as i32);
+                pivots.push(i as i32);
+                
+                return pivots;
+            }
+            i+=1;
+        }
+        pivots
     }
 }
 
@@ -12,19 +25,19 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = Solution::two_sum(Vec::new([2_i32,7,11,15]), 9_i32);
-        assert_eq!(result, Vec::new([0,1]));
+        let result = Solution::two_sum(Vec::from([2_i32,7,11,15]), 9_i32);
+        assert_eq!(result, Vec::from([0_i32,1]));
     }
 
     #[test]
-    fn it_works() {
-        let result = Solution::two_sum(Vec::new([3_i32,2,4]), 6_i32);
-        assert_eq!(result, Vec::new([1,2]));
+    fn it_works2() {
+        let result = Solution::two_sum(Vec::from([3_i32,2,4]), 6_i32);
+        assert_eq!(result, Vec::from([1_i32,2]));
     }
 
     #[test]
-    fn it_works() {
-        let result = Solution::two_sum(Vec::new([3_i32,3]), 6_i32);
-        assert_eq!(result, Vec::new([0,1]));
+    fn it_works3() {
+        let result = Solution::two_sum(Vec::from([3_i32,3]), 6_i32);
+        assert_eq!(result, Vec::from([0_i32,1]));
     }
 }
